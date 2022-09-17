6257ec449288810ea244040866cc23c0acddf7ab
Luckkk1 <dldbstjr4444@gmail.com>
2022-09-17 20:54:34 +0900

    Update Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -16,7 +16,7 @@ const Layout = props => {
     return () => {
       clearTimeout(reset);
     };
-  }, [isCopied, dispatch, colorActions]);
+  }, [isCopied, dispatch]);
   return (
     <div>
       <Header />
