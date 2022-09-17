a13eb1d2c4a245f8e29ffe81eeca5e0175bc15b8
Luckkk1 <dldbstjr4444@gmail.com>
2022-09-17 21:13:10 +0900

    remove

--- pages/_app.js
+++ pages/_app.js
@@ -8,21 +8,6 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Head>
-        <title>
-          Pick The Color | Color palette that finds the opposite color
-        </title>
-        <meta
-          name="google-site-verification"
-          content="aj3LlJ97q8xn0V6bc60sO1gTDg4PYSrgyJSIROzeF8E"
-        />
-        <meta name="author" content="Ys Lee" />
-        <meta
-          name="description"
-          content="Share your own color palette with a simple color picker."
-        />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
